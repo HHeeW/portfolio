0619d8d4afdd9f04586c2d364ad3ec964a59432e
HeeWon <sun4472@naver.com>
2022-07-27 09:46:42 +0900

    2022-07-27

--- heewon/src/component/Main.jsx
+++ heewon/src/component/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
       <Background/>
       <div className="Box">
         <div data-aos="fade-up-right">
-          <h1 className='name'>안녕하세요,</h1>/
+          <h1 className='name'>안녕하세요,</h1>
         </div>
         <div data-aos="fade-up-left">
           <h1 className='name'>저는 황희원입니다.</h1>
